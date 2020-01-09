50e8b21aafed7727c21de9bf625ebd03cea250e8
Justin White <kyjus25@gmail.com>
2020-01-09 10:55:25 -0600

    Oops. Guess this got missed just now

--- src/main/gameengine/Level.java
+++ src/main/gameengine/Level.java
@@ -1,4 +1,8 @@
 package main.gameengine;
 
+import javafx.scene.Group;
+
 public class Level {
+    public Group level = new Group();
+    public String name = this.getClass().getSimpleName().toLowerCase();
 }
